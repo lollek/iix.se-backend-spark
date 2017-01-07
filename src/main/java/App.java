1cba7994b607f28fe863f9bd56d95fea086c4bf9
Olle Kvarnström <olle+git@iix.se>
2017-01-07 11:43:51 +0100

    Add Notes with index/show

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,17 @@
+import Database.Database;
+import controller.NotesController;
+
+import java.sql.SQLException;
+
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) {
-        get("/", (req, res) -> "Hello, World!");
+
+    public static void main(String[] args) throws SQLException {
+        Database.init();
+
+        get("/notes", NotesController.index);
+        get("/notes/:id", NotesController.show);
     }
 }
 
---
+++ src/main/java/model/Note.java
@@ -0,0 +1,53 @@
+package model;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@DatabaseTable(tableName = "notes")
+public class Note implements Serializable {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField
+    private String title;
+
+    @DatabaseField
+    private String text;
+
+    @DatabaseField(dataType = DataType.DATE_STRING, format = "yyyy-MM-dd")
+    private Date date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
