1f6af276c14dbd8646bff5c3cc88699838f7d82e
Olle Kvarnström <olle+git@iix.se>
2017-01-22 15:52:43 +0100

    Cache steam wishlist for an hour

--- src/main/java/controller/GamesController.java
+++ src/main/java/controller/GamesController.java
@@ -1,11 +1,27 @@
 package controller;
 
+import model.Game;
 import service.JsonService;
 import service.SteamWishlistService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GamesController {
-    public static Route index = (Request request, Response response) -> JsonService.toJson(SteamWishlistService.getWishlist("loll3k"));
+    private static List<Game> gameList = new ArrayList<>();
+    private static LocalDateTime gameListUpdate;
+
+    private static synchronized List<Game> getGameList() {
+        if (gameListUpdate == null || gameListUpdate.isBefore(LocalDateTime.now().minusHours(1))) {
+            gameList = SteamWishlistService.getWishlist("loll3k");
+            gameListUpdate = LocalDateTime.now();
+        }
+        return gameList;
+    }
+
+    public static Route index = (Request request, Response response) -> JsonService.toJson(getGameList());
 }
