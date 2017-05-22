18bd7785bbc97ce32e72d076a434d085552a75a4
Olle Kvarnström <olle+git@iix.se>
2017-05-22 21:22:02 +0200

    Change SteamWishlistService from java to kotlin

---
+++ src/main/kotlin/service/SteamWishlistService.kt
@@ -0,0 +1,42 @@
+package service
+
+import model.Game
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import java.util.*
+
+class SteamWishlistService {
+    companion object {
+        private fun gameFromRow(row: Element): Game {
+            val image: String = row
+                    .select("div.gameListRowLogo").first()
+                    .select("a[href]").first()
+                    .select("img[src]").first()
+                    .attr("src")
+
+            val wishlistRowItem: Element = row.select("div.wishlistRowItem").first()
+            val name: String = wishlistRowItem.select("h4.ellipsis").first().text()
+
+            val gameListPriceData: Element = wishlistRowItem.select("div.gameListPriceData").first()
+            val link: String = gameListPriceData
+                    .select("div.storepage_btn_ctn").first()
+                    .select("a[href]").first()
+                    .attr("href")
+
+            return Game(name, link, image)
+        }
+
+        fun getWishlist(username: String): List<Game> {
+            try {
+                return Jsoup
+                        .connect("https://steamcommunity.com/id/$username/wishlist")
+                        .get()
+                        .select("div#wishlist_items > div.wishlistRow")
+                        .map { gameFromRow(it) }
+            } catch (e: Exception) {
+                LogService.logException(e)
+                return ArrayList()
+            }
+        }
+    }
+}
